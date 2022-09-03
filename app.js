a82fd2bf4556b10b4223beb264cf607373f7f010
Unnati Makrariya <unnatimakrariya@gmail.com>
2022-09-03 17:30:24 +0530

    input output processing

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ var btnTranslate = document.querySelector("#btn-translate");
 // btnTranslate.addEventListener("click", function clickEventHandler(){
 // console.log("clicked")})
 var txtinput=document.querySelector("#txt-input");
-console.log( txtinput )
+var outputDiv= document.querySelector("#output");
+
+
+console.log( outputDiv );
 
 function clickHandler(){
-    console.log("clicked!")
+    console.log("clicked!");
     console.log("input", txtinput.value);
-}
+    outputDiv.innerText="banana " + txtinput.value
+};
 
 btnTranslate.addEventListener('click',clickHandler)
